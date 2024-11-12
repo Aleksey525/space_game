f354a19fc0ad0a9285f5781694223fa89c27964a
Aleksey525 <alex_tolchin@mail.ru>
2024-11-12 18:29:23 +0300

    connected the shot animation

--- space_game.py
+++ space_game.py
@@ -29,6 +29,36 @@ async def blink(canvas, row, column, animation_offset, symbol='*'):
             await asyncio.sleep(0)
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 def draw(canvas):
     curses.curs_set(0)
     canvas.border()
@@ -39,6 +69,8 @@ def draw(canvas):
               random.randint(1, 20),
               random.choice(list(SYMBOLS_FOR_STARS))) for _ in range(100)
     ]
+
+    coroutines.append(fire(canvas, rows / 2, columns / 2))
 
     while True:
         for coroutine in coroutines.copy():
